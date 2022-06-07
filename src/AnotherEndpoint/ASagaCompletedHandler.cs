968936b5b4a0c1e10e969b145b66ee6750ba5633
Mauro Servienti <mauro.servienti@gmail.com>
2022-06-07 14:36:00 +0200

    Add some complexity and one more failing scenario

---
+++ src/AnotherEndpoint/ASagaCompletedHandler.cs
@@ -0,0 +1,15 @@
+﻿using Messages;
+using NServiceBus;
+
+namespace AnotherEndpoint
+{
+    class ASagaCompletedHandler : IHandleMessages<ASagaCompleted>
+    {
+        public Task Handle(ASagaCompleted message, IMessageHandlerContext context)
+        {
+            Console.WriteLine($"Saga completed {message.SomeId}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
